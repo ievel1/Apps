3cd039e73e93d4a95a455b47b25de33616a6c6c0
william <william@space-inventor.com>
2023-10-11 14:08:08 +0200

    added a repo for weather icons

--- Weather/weather.py
+++ Weather/weather.py
@@ -1,6 +1,9 @@
 import requests
 import json
 import datetime
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
 
 
 
@@ -16,3 +19,18 @@ response = requests.get(url)
 if response.status_code == 200:
     data = response.json()
     print(data)
+
+
+# Create the main Tkinter window
+root = tk.Tk()
+root.title("Weather Icons Example")
+
+# Create a ttk.Style object and load the CSS
+style = ttk.Style()
+style.configure("Weather.TLabel", font=("Weather Icons", 24))
+
+# Create a label with a weather icon
+weather_label = ttk.Label(root, text="\uf00d", style="Weather.TLabel")
+weather_label.pack()
+
+root.mainloop()
